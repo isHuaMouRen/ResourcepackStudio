eaf6326ce2730070706247ea0ab1d7f4c25d4394
HuaMouRen <2825338723@qq.com>
2026-02-11 11:52:05 +0800

    chore: 移动主窗口

--- RresourcepackStudio/App.xaml.cs
+++ RresourcepackStudio/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using RresourcepackStudio.Windows;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,15 @@ namespace RresourcepackStudio
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var window = new WindowMain();
+            window.Show();
+
+            Current.MainWindow = window;
+        }
     }
 
 }
