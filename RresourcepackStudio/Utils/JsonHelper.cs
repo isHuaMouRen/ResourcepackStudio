87223d026cc064bae34b3e7ef429c8c8c7cf361c
HuaMouRen <2825338723@qq.com>
2026-02-11 13:14:12 +0800

    feat(utils): 添加JsonHelper工具

---
+++ RresourcepackStudio/Utils/JsonHelper.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace RresourcepackStudio.Utils
+{
+    public static class JsonHelper
+    {
+        public static void WriteJson<T>(string path, T jsonObj)
+        {
+            using var streamWriter = new StreamWriter(path);
+            using var jsonWriter = new JsonTextWriter(streamWriter)
+            {
+                Formatting = Formatting.Indented,
+                Indentation = 4,
+                IndentChar = ' '
+            };
+            new JsonSerializer().Serialize(jsonWriter, jsonObj);
+            streamWriter.Flush();
+        }
+
+        public static T? ReadJson<T>(string path)
+        {
+            if (!File.Exists(path))
+                return default;
+
+            string json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
