48920f2bdfa06236267187c134d688c515b8a2fb
HuaMouRen <2825338723@qq.com>
2026-02-11 18:40:53 +0800

    feat(globals): 添加全局类

---
+++ RresourcepackStudio/Classes/Globals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Reflection;
+using RresourcepackStudio.Classes.Configs;
+
+namespace RresourcepackStudio.Classes
+{
+    public static class Globals
+    {
+        public static string Version = "1.0.0-beta.1";//版本号
+        public static string ExecuteDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;//执行目录
+
+        public static JsonProjectFileConfig.Index? CurrentProject = null;//当前项目文件
+        public static string? CurrentProjectDirectory = null;//当前项目目录
+    }
+}
