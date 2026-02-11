5fc6603b61889bcdcfa0a8c6d67b68688e7bd555
HuaMouRen <2825338723@qq.com>
2026-02-11 13:34:35 +0800

    feat(utils): 添加两个工具类

---
+++ RresourcepackStudio/Utils/UI/DialogManager.cs
@@ -0,0 +1,41 @@
+﻿using ModernWpf.Controls;
+
+namespace RresourcepackStudio.Utils.UI
+{
+    public static class DialogManager
+    {
+        private static readonly SemaphoreSlim DialogSemaphore = new(1, 1);
+
+        public static async Task<ContentDialogResult> ShowDialogAsync(
+            ContentDialog dialog,
+            Action? primaryCallback = null,
+            Action? secondaryCallback = null,
+            Action? closeCallback = null)
+        {
+
+            await DialogSemaphore.WaitAsync();
+            try
+            {
+                var result = await dialog.ShowAsync();
+
+                // 执行回调
+                switch (result)
+                {
+                    case ContentDialogResult.Primary: primaryCallback?.Invoke(); break;
+                    case ContentDialogResult.Secondary: secondaryCallback?.Invoke(); break;
+                    default: closeCallback?.Invoke(); break;
+                }
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return ContentDialogResult.None;
+            }
+            finally
+            {
+                DialogSemaphore.Release();
+            }
+        }
+    }
+}
